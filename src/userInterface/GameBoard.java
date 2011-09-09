e7b1af78a387f246b0780885ebdc8b1d3659189f
Sixten Hilborn <hilborn90@hotmail.com>
2011-09-09 18:00:22 +0200

    Putting userInterface in its package aswell

--- src/userInterface/GameBoard.java
+++ src/userInterface/GameBoard.java
@@ -1,3 +1,5 @@
+package userInterface;
+
 import javax.swing.*;
 import java.awt.*;
 
--- src/userInterface/MainWindow.java
+++ src/userInterface/MainWindow.java
@@ -1,24 +1,23 @@
-import javax.swing.*;
+package userInterface;
 
-class MainWindow extends JFrame
+import javax.swing.*;
+import gameLogic.*;
+
+public class MainWindow extends JFrame
 {
+	private Session session;
 	private GameBoard gameBoard;
 	
 	public MainWindow()
 	{
 		gameBoard = new GameBoard(400, 300);
-		
-		setLayoutManager(null);
-		
+				
 		add(gameBoard);
 		pack();
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
+		setResizable(false);
 	}
 	
-	public static void main(String[] args)
-	{
-		new MainWindow();
-	}
 }
