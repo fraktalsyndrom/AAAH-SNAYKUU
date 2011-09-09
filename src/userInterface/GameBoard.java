9e0ffb06ff1d5aed7c2fbd948cca8bf59d687265
Sixten Hilborn <hilborn90@hotmail.com>
2011-09-09 18:38:56 +0200

    Painting a grid based on gameLogic.Session

--- src/userInterface/GameBoard.java
+++ src/userInterface/GameBoard.java
@@ -2,16 +2,49 @@ package userInterface;
 
 import javax.swing.*;
 import java.awt.*;
+import gameLogic.*;
 
 class GameBoard extends JComponent
 {
-	public GameBoard(int width, int height)
-	{		
+	private Session session;
+	private int pixelsPerUnit;
+	
+	public GameBoard(Session session, int pixelsPerUnit)
+	{
+		this.session = session;
+		this.pixelsPerUnit = pixelsPerUnit;
+		
+		int width = session.getBoard().getWidth() * pixelsPerUnit;
+		int height = session.getBoard().getHeight() * pixelsPerUnit;
+		
 		setSize(width, height);
 		
 		Dimension d = new Dimension(width, height);
 		setMaximumSize(d);
 		setMinimumSize(d);
 		setPreferredSize(d);
+	}
+	
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		
+		Board board = session.getBoard();
+		int width = pixelsPerUnit * board.getWidth();
+		int height = pixelsPerUnit * board.getWidth();
+		
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.setColor(Color.WHITE);
+		
+		for (int x = 1; x < board.getWidth(); ++x)
+		{
+			g.drawLine(x * pixelsPerUnit, 0, x * pixelsPerUnit, height);
+		}
+		
+		for (int y = 1; y < board.getHeight(); ++y)
+		{
+			g.drawLine(0, y * pixelsPerUnit, width, y * pixelsPerUnit);
+		}
 	}
 }
