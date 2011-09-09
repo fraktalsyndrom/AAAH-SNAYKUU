5b39905b3340e550a4466c9c378f193b316240de
Sixten Hilborn <hilborn90@hotmail.com>
2011-09-09 17:42:56 +0200

    My window is amazing

---
+++ src/userInterface/GameBoard.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+class GameBoard extends JComponent
+{
+	public GameBoard(int width, int height)
+	{		
+		setSize(width, height);
+		
+		Dimension d = new Dimension(width, height);
+		setMaximumSize(d);
+		setMinimumSize(d);
+		setPreferredSize(d);
+	}
+}
--- src/userInterface/MainWindow.java
+++ src/userInterface/MainWindow.java
@@ -1,7 +1,24 @@
+import javax.swing.*;
 
-class MainWindow
+class MainWindow extends JFrame
 {
+	private GameBoard gameBoard;
+	
+	public MainWindow()
+	{
+		gameBoard = new GameBoard(400, 300);
+		
+		setLayoutManager(null);
+		
+		add(gameBoard);
+		pack();
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
 	public static void main(String[] args)
 	{
+		new MainWindow();
 	}
 }
