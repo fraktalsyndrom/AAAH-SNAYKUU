7b931ae5a275051b8c1945579ff77a1725c0a74d
Arian Jafari <aria-jaf@dsv.su.se>
2011-09-12 17:29:09 +0200

    Moved a lot of functionality out of Snake. Removed deprecated move()-method. Snakes no longer keep track of where they are themselves, although this may be subject to change soon. Still experimental changes.

--- src/gameLogic/Snake.java
+++ src/gameLogic/Snake.java
@@ -1,12 +1,10 @@
 package gameLogic;
 
-
 import java.util.*;
 
 public class Snake
 {
 	private String name;
-	private LinkedList<SnakeSegment> segments = new LinkedList<SnakeSegment>();
 	private Brain brain;
 	private Direction direction;
 	private boolean grow = false;
@@ -16,58 +14,11 @@ public class Snake
 		this.name = name;
 		this.brain = brain;
 		this.direction = new Direction(Direction.NORTH);
-		this.segments.add(new SnakeSegment(position, null));
-	}
-	
-	public SnakeSegment getHead()
-	{
-		return segments.get(0);
-	}
-	
-	public SnakeSegment getTail()
-	{
-		return segments.getLast();
-	}
-	
-	public LinkedList<SnakeSegment> getSegments()
-	{
-		return segments;
-	}
-	
-	/**
-	 * NOTE: Currently not used nor working. Functionality has instead been moved to moveSnake() in Session. 
-	 */
-	void move(GameState currentGameState)
-	{
-		direction = brain.getNextMove(currentGameState);
-		SnakeSegment head = getHead();
-		
-		Position newHeadPosition = direction.calculateNextPosition(head.getPosition());
-		//~ segments.addFirst(new SnakeSegment(session.getBoard(), newHeadPosition, head));
-		
-		if (grow)
-		{
-			grow = false;
-			return;
-		}
-		
-		SnakeSegment tail = getTail();
-		segments.removeLast();
 	}
 	
 	Direction getNextMove(GameState currentGameState)
 	{
 		return brain.getNextMove(currentGameState);
-	}
-	
-	void updatePosition(LinkedList<SnakeSegment> newSegments)
-	{
-		segments = newSegments;
-	}
-	
-	void growOneUnitOfLengthNextTimeThisSnakeMoves()
-	{
-		grow = true;
 	}
 	
 	Brain getBrain()
