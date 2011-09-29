19ac283b67f5ae996833ec6666ab6558d3c4bdf6
Arian Jafari <aria-jaf@dsv.su.se>
2011-09-29 19:58:24 +0200

    Direction is now an enum. Relative directions have been removed, and only absolute directions (NORTH, SOUTH et al) remain.

--- src/gameLogic/Direction.java
+++ src/gameLogic/Direction.java
@@ -1,64 +1,14 @@
 package gameLogic;
 
-
-public class Direction
+public enum Direction
 {
-	public static final int NORTH = 1;
-	public static final int WEST = 2;
-	public static final int SOUTH = 3;
-	public static final int EAST = 4;
-	public static final int FORWARD = 5;
-	public static final int LEFT = 6;
-	public static final int RIGHT = 7;
-
-	private int direction;
-
-	public Direction(int direction)
-	{
-		if (direction < NORTH || direction > EAST)
-			throw new IllegalArgumentException("We need an absolute direction!");
-		
-		this.direction = direction;
-	}
-	
-	public Direction(int originalDirection, int relativeDirection)
-	{
-		if (originalDirection < NORTH || originalDirection > EAST)
-			throw new IllegalArgumentException("We need an valid original direction!");
-		
-		direction = originalDirection;
-		
-		switch (relativeDirection)
-		{
-			case LEFT:
-				--direction;
-				break;
-			
-			case RIGHT:
-				++direction;
-				break;
-			
-			case FORWARD:
-				break;
-			
-			default:
-				throw new IllegalArgumentException("wat, gief relative direction plz");
-		}
-		
-		if (direction < NORTH)
-			direction = EAST;
-		
-		if (direction > EAST)
-			direction = NORTH;
-			
-		
-	}
+	NORTH, WEST, SOUTH, EAST;
 	
 	public Position calculateNextPosition(Position oldPosition)
 	{
 		int x = oldPosition.getX(), y = oldPosition.getY();
 		
-		switch (direction)
+		switch (this)
 		{
 			case WEST:
 				--x;
@@ -79,35 +29,26 @@ public class Direction
 		
 		return new Position(x, y);
 	}
-	
-	public boolean equals(Object other)
-	{
-		if (!(other instanceof Direction))
-			return false;
-		
-		Direction otherDirection = (Direction)other;
-		
-		return direction == otherDirection.direction;
-	}
-	
+
 	static Direction getDirectionFromPositionToPosition(Position from, Position to)
 	{
 		if (from.equals(to))
 			throw new IllegalArgumentException("Don't force us to divide by zero please");
+			//~ TODO: Wouldn't this be dividing by one and not zero? Investigate.
 		
 		if (from.getX() == to.getX())
 		{
 			if (from.getY() < to.getY())
-				return new Direction(SOUTH);
+				return Direction.SOUTH;
 			else if (from.getY() > to.getY())
-				return new Direction(NORTH);
+				return Direction.NORTH;
 		}
 		else if (from.getY() == to.getY())
 		{
 			if (from.getX() < to.getX())
-				return new Direction(EAST);
+				return Direction.EAST;
 			else if (from.getX() > to.getX())
-				return new Direction(WEST);
+				return Direction.WEST;
 		}
 		
 		throw new IllegalArgumentException("The two points must be.");
