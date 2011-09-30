f2c046d7371c11bf7464150f2dbc17c9b5d5caa9
Sixten Hilborn <hilborn90@hotmail.com>
2011-10-01 00:32:23 +0200

    Adding some more stuff to the game settings tab

--- src/userInterface/GameSettingsPanel.java
+++ src/userInterface/GameSettingsPanel.java
@@ -10,11 +10,47 @@ import gameLogic.*;
 class GameSettingsPanel extends JPanel
 {
 	private JLabel lolText;
+	private JTextField boardWidth;
+	private JTextField boardHeight;
 	
 	public GameSettingsPanel()
 	{
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		lolText = new JLabel("GAME SETTINGS");
-		
+				
 		add(lolText);
+		
+		initializeBoardSizeSettings();
+	}
+	
+	private void initializeBoardSizeSettings()
+	{
+		boardWidth = new JTextField("10");
+		boardHeight = new JTextField("10");
+		
+		boardWidth.setColumns(4);
+		boardHeight.setColumns(4);
+		
+		JPanel widthPanel = new JPanel();
+		JPanel heightPanel = new JPanel();
+		
+		widthPanel.add(new JLabel("Board width:"));
+		widthPanel.add(boardWidth);
+		heightPanel.add(new JLabel("Board height:"));
+		heightPanel.add(boardHeight);
+				
+		add(widthPanel);
+		add(heightPanel);
+	}
+	
+	
+	public int getBoardWidth()
+	{
+		return Integer.parseInt(boardWidth.getText());
+	}
+	
+	public int getBoardHeight()
+	{
+		return Integer.parseInt(boardHeight.getText());
 	}
 }
