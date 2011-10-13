938fcde080156b555ae2da790f5b20a04dca12a0
Arian Jafari <aria-jaf@dsv.su.se>
2011-10-13 22:17:07 +0200

    Documenting GameObject.

--- src/gameLogic/GameObject.java
+++ src/gameLogic/GameObject.java
@@ -1,14 +1,26 @@
 package gameLogic;
 
+/**
+ * This class represents an object that exists on the game board. Each GameObject
+ * has a GameObjectType describing which kind of object it is (e.g. Fruit, Wall etc).
+ * 
+ * @see 	GameObjectType
+ */
 
 public class GameObject
 {	
 	GameObjectType type;
-	public GameObject(GameObjectType type)
+	
+	GameObject(GameObjectType type)
 	{
 		this.type = type;
 	}
 	
+	/**
+	 * Gets the GameObjectType of this object.
+	 * 
+	 * @return	This object's GameObjectType.
+	 */
 	public GameObjectType getType()
 	{
 		return type;
