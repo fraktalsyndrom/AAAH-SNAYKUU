63113e570ac5de23a9c616c9b176668b1a9bbd1f
Arian Jafari <aria-jaf@dsv.su.se>
2011-10-13 20:06:30 +0200

    Beginning to thoroughly document the methods in GameState.

--- src/gameLogic/GameState.java
+++ src/gameLogic/GameState.java
@@ -18,23 +18,39 @@ public class GameState
 	}
 	
 	/**
-	 * 
+	 * Returns a Set containing all snakes in the game, both dead ones and alive ones.
+	 *
+	 * @return 	A Set<Snake> containing all snakes.
+	 * @see		Snake
 	 */
 	public Set<Snake> getSnakes()
 	{
 		return snakes;
 	}
 	
+	/**
+	 * Returns a Board object, which constists of a 2D-array of Square objects.
+	 *
+	 * @return 	A representation of the current game board.
+	 * @see		Board
+	 */
 	public Board getBoard()
 	{
 		return board;
 	}
 	
+	/**
+	 * Method for getting the current game metadata, containing (among other things) 
+	 * time until the next fruit spawns and time until snake growth.
+	 *
+	 * @return 	The current Metadata object.
+	 * @see		Metadata
+	 */
 	public Metadata getMetadata()
 	{
 		return metadata;
 	}
-	
+
 	public ErrorState getErrorState()
 	{
 		return errorState;
@@ -42,8 +58,16 @@ public class GameState
 	
 	
 	/*
-	 * Method stub, intended to eventually calculate whether or not a
-	 * given snake will collide next turn if it moves in a particular direction.
+	 * This method can be used to help calculate whether or not a given snake will collide next
+	 * turn if it continues in a given direction. It looks at the square the snake will end up in,
+	 * and then checks if that square contains a lethal object. Note that this method returning false 
+	 * does NOT guarantee that the snake will survive. For example, it is possible that a two
+	 * snakes will move into an empty square during the same turn, causing the death of
+	 * them both.
+	 * 
+	 * @param	snake	The snake you wish you perform the check for.
+	 * @param	dir		The hypothetical direction in which the snake moves.
+	 * @return 	A boolean: true if the next position contains a lethal object, false if not.
 	 */
 	public boolean willCollide(Snake snake, Direction dir)
 	{
@@ -52,25 +76,22 @@ public class GameState
 		return (board.getSquare(nextHeadPosition).isLethal());
 	}
 	
-	
-	
 	/**
-	 * Method stubs intended to eventually be of assistance during the planning phase.
-	 * Do we want the methods to be here?
+	 * This method can be used to calculate the distance between two positions.
+	 * 
+	 * @param 	from		The position from which you wish to calculate the distance.
+	 * @param	to		The position to which you wish to calculate the distance.
+	 * @return	An integer representing the distance between two positions.
 	 */
 	public static int distanceBetween(Position from, Position to)
 	{
 		int distance = 0;
-		int fromX = from.getX();
-		int fromY = from.getY();
-		int toX = to.getX();
-		int toY = to.getY();
 		
 		// Calculate distance in the x-axis
-		distance += Math.abs(fromX - toX);
+		distance += Math.abs(from.getX() - to.getX());
 		
 		// Calculate distance in the y-axis
-		distance += Math.abs(fromY - toY);
+		distance += Math.abs(from.getY() - to.getY());
 
 		return distance;
 	}
