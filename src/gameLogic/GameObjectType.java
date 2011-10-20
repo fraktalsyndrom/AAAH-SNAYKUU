5eaab949278bca6f2ed7bcdcfb8053ad3e7d0372
Arian Jafari <aria-jaf@dsv.su.se>
2011-10-20 21:53:12 +0200

    Documenting GameObjectType.

--- src/gameLogic/GameObjectType.java
+++ src/gameLogic/GameObjectType.java
@@ -1,6 +1,10 @@
 package gameLogic;
 
 /**
+ * Represents a type of object that may be present on the board. Examples include
+ * Fruit and Wall. Instances of some GameObjectTypes are lethal to collide with,
+ * while others might give points if "eaten".
+ *
  * @author	Sixten Hilborn
  * @author	Arian Jafari
  */
@@ -10,6 +14,10 @@ public class GameObjectType
 	private boolean isLethal;
 	private int value;
 	
+	/**
+	 * Constructs a GameObjectType object with the given name, lethality and
+	 * numeric point value.
+	 */
 	public GameObjectType(String name, boolean isLethal, int value)
 	{
 		this.name = name;
@@ -24,11 +32,23 @@ public class GameObjectType
 		value = 0;
 	}
 	
+	/**
+	 * Returns the name of this GameObjectType, for instance "Fruit" or "Wall".
+	 * 
+	 * @return	the name of this GameObjectType.
+	 */
 	public String getName()
 	{
 		return name;
 	}
 	
+	/**
+	 * Gets the number of points objects of this type are worth. The default value
+	 * for fruit is 1.
+	 * 
+	 * @return	the number of points that are granted by "eating" objects of
+	*			this GameObjectType.
+	 */
 	public int getValue()
 	{
 		return value;
