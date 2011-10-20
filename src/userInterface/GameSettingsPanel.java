7024b6704a7101c5d8b812f1c668500943586fe4
Arian Jafari <aria-jaf@dsv.su.se>
2011-10-20 21:05:21 +0200

    Added text fields for setting fruit frequency, growth frequency and fruit goal.

--- src/userInterface/GameSettingsPanel.java
+++ src/userInterface/GameSettingsPanel.java
@@ -12,6 +12,9 @@ class GameSettingsPanel extends JPanel
 	private JLabel lolText;
 	private JTextField boardWidth;
 	private JTextField boardHeight;
+	private JTextField fruitFrequency;
+	private JTextField growthFrequency;
+	private JTextField fruitGoal;
 	private JTextField gameSpeed;
 	
 	public GameSettingsPanel()
@@ -34,20 +37,43 @@ class GameSettingsPanel extends JPanel
 	{
 		boardWidth = new JTextField("15");
 		boardHeight = new JTextField("15");
+		fruitGoal = new JTextField("5");
+		fruitFrequency = new JTextField("10");
+		growthFrequency = new JTextField("3");
 		
 		boardWidth.setColumns(4);
 		boardHeight.setColumns(4);
+		fruitGoal.setColumns(4);
+		fruitFrequency.setColumns(4);
+		growthFrequency.setColumns(4);
 		
 		JPanel widthPanel = new JPanel();
 		JPanel heightPanel = new JPanel();
 		
+		JPanel goalPanel = new JPanel();
+		JPanel freqPanel = new JPanel();
+		JPanel growthPanel = new JPanel();
+		
 		widthPanel.add(new JLabel("Board width:"));
 		widthPanel.add(boardWidth);
+		
+		goalPanel.add(new JLabel("Fruit goal"));
+		goalPanel.add(fruitGoal);
+		
+		freqPanel.add(new JLabel("Fruit freq."));
+		freqPanel.add(fruitFrequency);
+		
+		growthPanel.add(new JLabel("Growth freq."));
+		growthPanel.add(growthFrequency);
+		
 		heightPanel.add(new JLabel("Board height:"));
 		heightPanel.add(boardHeight);
 				
 		add(widthPanel);
 		add(heightPanel);
+		add(goalPanel);
+		add(freqPanel);
+		add(growthPanel);
 	}
 	
 	
@@ -55,12 +81,12 @@ class GameSettingsPanel extends JPanel
 	{
 		int width = Integer.parseInt(boardWidth.getText()) + 2;
 		int height = Integer.parseInt(boardHeight.getText()) + 2;
-		int growthFrequency = 3;
-		int fruitFrequency = 10;
+		int growth = Integer.parseInt(growthFrequency.getText());
+		int fruitFreq = Integer.parseInt(fruitFrequency.getText());
 		int thinkingTime = 100;
-		int fruitGoal = 5;
+		int fruitG = Integer.parseInt(fruitGoal.getText());
 		
-		Metadata metadata = new Metadata(width, height, growthFrequency, fruitFrequency, thinkingTime, fruitGoal);
+		Metadata metadata = new Metadata(width, height, growth, fruitFreq, thinkingTime, fruitG);
 		return metadata;
 	}
 	
