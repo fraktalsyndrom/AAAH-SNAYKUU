980b2f922fef3428714e73590bf528a1fc3c94a8
Sixten Hilborn <hilborn90@hotmail.com>
2011-11-03 21:16:34 +0100

    Updating the class loader to be using code from http://java.sun.com/developer/onlineTraining/Security/Fundamentals/magercises/ClassLoader/help.html. This seems to work well with bots which are using multiple classes.

--- src/userInterface/BotClassLoader.java
+++ src/userInterface/BotClassLoader.java
@@ -65,7 +65,7 @@ class BotClassLoader extends ClassLoader
 		{
 			try
 			{
-				Class<?> brainClass = loadBotClass(url, name);
+				Class<?> brainClass = loadClass("bot."+name, true); //(url, name);
 				brainClassInfo = new BrainClassInfo(brainClass, url);
 				loadedBrainClasses.put(name, brainClassInfo);
 			}
@@ -83,22 +83,90 @@ class BotClassLoader extends ClassLoader
 	{
 		try
 		{				
-			//URL url = new URL("file:" + path);
 			URLConnection urlConnection = url.openConnection();
 			InputStream input = urlConnection.getInputStream();
 			
 			byte[] classData = readBufferFromStream(input);
-			
-			return defineClass("bot."+name, classData, 0, classData.length);
+						
+			//return defineClass("bot."+name, classData, 0, classData.length);
+			Class newClass = defineClass(null, classData, 0, classData.length);
+			if (newClass != null)
+				return newClass;
+				
 		}
 		catch (IOException e)
 		{
 			System.out.println(e);
 		}
 
-		return null;
+		throw new ClassNotFoundException(name);
 	}
 	
+	protected Class loadClass(String name, boolean resolve) throws ClassNotFoundException
+	{
+		Class c = findLoadedClass (name);
+		if (c == null)
+		{
+			try
+			{
+				c = findSystemClass(name);
+			}
+			catch (Exception e)
+			{
+				// Ignore these
+			}
+		}
+		
+		if (c == null)
+		{
+			// Convert class name argument to filename
+			// Convert package names into subdirectories
+			String filename = name.replace ('.', File.separatorChar) + ".class";
+			
+			try
+			{
+				byte[] data = loadClassData(filename);
+
+				c = defineClass (name, data, 0, data.length);
+				if (c == null)
+					throw new ClassNotFoundException(name);
+			}
+			catch (IOException e)
+			{
+				throw new ClassNotFoundException ("Error reading file: " + filename);
+			}
+		}
+		
+		if (resolve)
+			resolveClass(c);
+		
+		return c;
+	}
+	
+	private byte[] loadClassData(String filename) throws IOException
+	{
+		// Create a file object relative to directory provided
+		File f = new File(".", filename);
+
+		// Get size of class file
+		int size = (int)f.length();
+
+		// Reserve space to read
+		byte buff[] = new byte[size];
+
+		// Get stream to read from
+		FileInputStream fis = new FileInputStream(f);
+		DataInputStream dis = new DataInputStream(fis);
+
+		// Read in data
+		dis.readFully(buff);
+
+		// close stream
+		dis.close();
+
+		// return data
+		return buff;
+	}
 	
 	private byte[] readBufferFromStream(InputStream input) throws IOException
 	{
