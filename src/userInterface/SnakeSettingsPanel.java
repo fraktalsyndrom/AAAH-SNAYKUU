05ded0fbbc5cb02895a0a3a89005bf488ea72a8b
Erik Thuning <erik@l956.dsv.su.se>
2011-11-08 17:22:34 +0100

    SnakeSettingsPanel has been updated with a cooler ui. The layout is slightly buggy atm, but all functionality is there. Work in progress.

--- src/userInterface/SnakeSettingsPanel.java
+++ src/userInterface/SnakeSettingsPanel.java
@@ -11,32 +11,115 @@ import java.util.TreeMap;
 import java.util.List;
 import java.util.ArrayList;
 import java.io.*;
+import static java.awt.GridBagConstraints.*;
 
 class SnakeSettingsPanel extends JPanel
 {
 	private JList snakeJList;
 	private JList brainJList;
-	private SnakeManagementPanel snakeManagementPanel;
+	private JButton addSnakeButton;
+	private JButton removeSnakeButton;
+	private JButton reloadAllBrainsButton;
 	private Map<String, String> snakes = new TreeMap<String, String>();
 	private Map<String, Class<? extends Brain>> brains = new TreeMap<String, Class<? extends Brain>>();
 	
 	public SnakeSettingsPanel()
 	{
-		setLayout(new BorderLayout());
+		 //~Cool version - looks better but is buggy atm
+		GridBagLayout gridbag = new GridBagLayout();
+		setLayout(gridbag);
+		
+		GridBagConstraints constraint = new GridBagConstraints();
+		
+		
+		constraint.fill = BOTH;
+		constraint.gridwidth = 2;
+		constraint.gridheight = 8;
+		constraint.weightx = 1.0;
+		
+		constraint.gridy = 0;
+		
+			constraint.gridx = 0;
+			constraint.anchor = WEST;
+			
+				snakeJList = new JList();
+				snakeJList.addMouseListener(new SnakeMouseListener());
 				
-		snakeJList = new JList();		
+				JScrollPane jsp1 = new JScrollPane(snakeJList);
+				gridbag.setConstraints(jsp1, constraint);
+				add(jsp1);
+				
+				
+			constraint.gridx = 3;
+			constraint.anchor = EAST;
+				
+				brainJList = new JList();
+				brainJList.addMouseListener(new BrainMouseListener());
+				
+				JScrollPane jsp2 = new JScrollPane(brainJList);
+				gridbag.setConstraints(jsp2, constraint);
+				add(jsp2);
+		
+		constraint.fill = NONE;
+		constraint.gridheight = 1;
+		constraint.weightx = 0.0;
+		
+			constraint.anchor = CENTER;
+			constraint.gridx = 2;
+			constraint.gridwidth = 1;
+			
+				constraint.gridy = 3;
+				
+					addSnakeButton = new JButton("←");
+					addSnakeButton.addActionListener(new AddSnakeListener());
+					gridbag.setConstraints(addSnakeButton, constraint);
+					add(addSnakeButton);
+				
+				constraint.gridy = 4;
+				
+					removeSnakeButton = new JButton("→");
+					removeSnakeButton.addActionListener(new RemoveSnakeListener());
+					gridbag.setConstraints(removeSnakeButton, constraint);
+					add(removeSnakeButton);
+			
+			constraint.anchor = SOUTH;
+			constraint.gridx = 0;
+			constraint.gridy = 9;
+			constraint.gridwidth = 5;
+			
+				reloadAllBrainsButton = new JButton("Reload all brains");
+				reloadAllBrainsButton.addActionListener(new ReloadBrainsListener());
+				gridbag.setConstraints(reloadAllBrainsButton, constraint);
+				add(reloadAllBrainsButton);
+		
+		
+		/* //~Vanilla version - ugly but works always.
+		super(new BorderLayout());
+		
+		JPanel west = new JPanel(new BorderLayout());
+		JPanel east = new JPanel(new BorderLayout());
+		JPanel center = new JPanel(new BorderLayout());
+		
+		add(west, BorderLayout.WEST);
+		add(east, BorderLayout.EAST);
+		add(center, BorderLayout.CENTER);
+		
+		snakeJList = new JList();
+		snakeJList.addMouseListener(new SnakeMouseListener());
+		west.add(new JScrollPane(snakeJList));
+		
 		brainJList = new JList();
+		brainJList.addMouseListener(new BrainMouseListener());
+		east.add(new JScrollPane(brainJList));
 		
-		snakeManagementPanel = new SnakeManagementPanel();		
+		addSnakeButton = new JButton("←");
+		addSnakeButton.addActionListener(new AddSnakeListener());
+		center.add(addSnakeButton, BorderLayout.NORTH);
 		
-		JPanel centerPanel = new JPanel();
-		centerPanel.add(new JScrollPane(snakeJList));
-		centerPanel.add(new JScrollPane(brainJList));
-		
-		add(centerPanel, BorderLayout.CENTER);
-		add(snakeManagementPanel, BorderLayout.NORTH);
-		
-		
+		removeSnakeButton = new JButton("→");
+		removeSnakeButton.addActionListener(new RemoveSnakeListener());
+		center.add(removeSnakeButton, BorderLayout.SOUTH);
+		*/
 		loadBrains();
 	}
 	
@@ -98,107 +181,81 @@ class SnakeSettingsPanel extends JPanel
 	}
 
 	
-	private class SnakeManagementPanel extends JPanel
+		
+	private class AddSnakeListener implements ActionListener
 	{
-		private JButton addSnakeButton;
-		private JButton removeSnakeButton;
-		private JButton reloadAllBrainsButton;
-		
-		public SnakeManagementPanel()
+		private String generateSnakeName(String name)
 		{
-			addSnakeButton = new JButton("Add snake");
-			addSnakeButton.addActionListener(new AddSnakeListener());
+			String snakeName = name;
+			int numberOfSnakesWithTheSameBrain = 1;
 			
-			removeSnakeButton = new JButton("Remove snake");
-			removeSnakeButton.addActionListener(new RemoveSnakeListener());
-			
-			reloadAllBrainsButton = new JButton("Reload all brains");
-			reloadAllBrainsButton.addActionListener(new ReloadBrainsListener());
-			
-			snakeJList.addMouseListener(new SnakeMouseListener());
-			brainJList.addMouseListener(new BrainMouseListener());
-			
-			add(addSnakeButton);
-			add(removeSnakeButton);
-			add(reloadAllBrainsButton);
-		}
-		
-		private class AddSnakeListener implements ActionListener
-		{
-			private String generateSnakeName(String name)
+			while (snakes.containsKey(snakeName))
 			{
-				String snakeName = name;
-				int numberOfSnakesWithTheSameBrain = 1;
-				
-				while (snakes.containsKey(snakeName))
-				{
-					++numberOfSnakesWithTheSameBrain;
-					snakeName = name + "#" + numberOfSnakesWithTheSameBrain;
-				}
-				
-				return snakeName;
+				++numberOfSnakesWithTheSameBrain;
+				snakeName = name + "#" + numberOfSnakesWithTheSameBrain;
 			}
 			
-			public void actionPerformed(ActionEvent event)
-			{
-				Object selectedObject = brainJList.getSelectedValue();
-				if (selectedObject == null)
-					return;
-				
-				
-				String name = selectedObject.toString();
-				
-				snakes.put(generateSnakeName(name), name);
-					
-				snakeJList.setListData(snakes.keySet().toArray());
-			}
+			return snakeName;
 		}
 		
-		private class RemoveSnakeListener implements ActionListener
+		public void actionPerformed(ActionEvent event)
 		{
-			public void actionPerformed(ActionEvent event)
-			{
-				Object selectedObject = snakeJList.getSelectedValue();
-				if (selectedObject == null)
-					return;
+			Object selectedObject = brainJList.getSelectedValue();
+			if (selectedObject == null)
+				return;
+			
+			
+			String name = selectedObject.toString();
+			
+			snakes.put(generateSnakeName(name), name);
 				
-				snakes.remove(selectedObject.toString());
-				
-				snakeJList.setListData(snakes.keySet().toArray());
-			}
+			snakeJList.setListData(snakes.keySet().toArray());
 		}
-		
-		private class ReloadBrainsListener implements ActionListener
+	}
+	
+	private class RemoveSnakeListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event)
 		{
-			public void actionPerformed(ActionEvent event)
-			{
-				String reloadedBrains = loadBrains();
-				JOptionPane.showMessageDialog(SnakeSettingsPanel.this, "Successfully reloaded:\n" + reloadedBrains);
-			}
+			Object selectedObject = snakeJList.getSelectedValue();
+			if (selectedObject == null)
+				return;
+			
+			snakes.remove(selectedObject.toString());
+			
+			snakeJList.setListData(snakes.keySet().toArray());
 		}
-		
-		private class SnakeMouseListener extends MouseAdapter
+	}
+	
+	private class ReloadBrainsListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event)
 		{
-			public void mouseClicked(MouseEvent e)
-			{
-				if (e.getClickCount() % 2 == 0)
-				{
-					new RemoveSnakeListener().actionPerformed(null);
-				}
-			}
+			String reloadedBrains = loadBrains();
+			JOptionPane.showMessageDialog(SnakeSettingsPanel.this, "Successfully reloaded:\n" + reloadedBrains);
 		}
-		
-		private class BrainMouseListener extends MouseAdapter
+	}
+	
+	private class SnakeMouseListener extends MouseAdapter
+	{
+		public void mouseClicked(MouseEvent e)
 		{
-			public void mouseClicked(MouseEvent e)
+			if (e.getClickCount() % 2 == 0)
 			{
-				if (e.getClickCount() % 2 == 0)
-				{
-					new AddSnakeListener().actionPerformed(null);
-				}
+				new RemoveSnakeListener().actionPerformed(null);
 			}
 		}
-
+	}
+	
+	private class BrainMouseListener extends MouseAdapter
+	{
+		public void mouseClicked(MouseEvent e)
+		{
+			if (e.getClickCount() % 2 == 0)
+			{
+				new AddSnakeListener().actionPerformed(null);
+			}
+		}
 	}
 	
 	
@@ -214,5 +271,4 @@ class SnakeSettingsPanel extends JPanel
 		}
 		return snakeMap;
 	}
-	
 }
