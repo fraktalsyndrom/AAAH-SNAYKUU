a9c5cebee793240fa263bd86abbd15b480653e93
Erik Thuning <thuning@dsv.su.se>
2011-11-11 23:45:15 +0100

    Added aggregation, customizability and political correctness.

--- src/userInterface/DeveloperPanel.java
+++ src/userInterface/DeveloperPanel.java
@@ -2,46 +2,131 @@ package userInterface;
 
 import javax.swing.*;
 import javax.swing.event.*;
-import java.awt.*;
 import java.awt.event.*;
-import gameLogic.*;
+import java.awt.Dimension;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
+import gameLogic.*;
 
 class DeveloperPanel extends JPanel
 {
 	private SettingsWindow settingsWindow;
 	private JButton statsButton;
+	private JTextField numberOfRuns;
 	
 	DeveloperPanel(SettingsWindow settingsWindow)
 	{
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
 		this.settingsWindow = settingsWindow;
 		
-		statsButton = new JButton("Fuck everyone in the face, here comes batman");
-		statsButton.addActionListener(new ActionListener() { public void actionPerformed(ActionEvent ae) { playOverNineThousandGames(); } } );
+		add(new JLabel("Number of runs:"));
+		
+		numberOfRuns = new JTextField("2");
+		numberOfRuns.setPreferredSize(new Dimension(200, 50));
+		JPanel jp = new JPanel();
+		jp.add(numberOfRuns);
+		add(jp);
+		
+		statsButton = new JButton("Run test games");
+		statsButton.addActionListener
+		(
+			new ActionListener()
+			{ 
+				public void actionPerformed(ActionEvent ae) 
+				{ 
+					playOverNineThousandGames(); 
+				} 
+			} 
+		);
+		
 		add(statsButton);
 	}
 	
 	private void playOverNineThousandGames()
 	{
-		final int numberOfGames = 20;
-		for (int currentGame = 0; currentGame < numberOfGames; ++currentGame)
+		try
 		{
-			System.out.println("Starting game #" + currentGame);
-			try
+			Session session = settingsWindow.generateSession();
+			
+			HashMap<String, Results> scores = new HashMap<String, Results>();
+			int numSnakes = session.getSnakes().size();
+			
+			for(Snake s : session.getSnakes())
 			{
-				Session session = settingsWindow.generateSession();
-				while (!session.hasEnded())
-					session.tick();
-				
-				System.out.println(session.getGameResult());
+				scores.put(s.getName(), new Results(numSnakes));
 			}
-			catch (Exception e)
+			
+			final int numberOfGames = Integer.parseInt(numberOfRuns.getText());
+			for (int currentGame = 0; currentGame < numberOfGames; ++currentGame)
 			{
-				System.out.println("Error: " + e);
+				System.out.println("Starting game #" + currentGame);
+				try
+				{
+					session = settingsWindow.generateSession();
+					while (!session.hasEnded())
+						session.tick();
+					
+					List<List<Snake>> result = session.getGameResult().getWinners();
+					
+					for(int i = 0; i < result.size(); ++i)
+					{
+						for(Snake s : result.get(i))
+						{
+							scores.get(s.getName()).addResult(i);
+						}
+					}
+				}
+				catch (Exception e)
+				{
+					System.out.println("Error: " + e);
+				}
+			}
+			
+			for(Map.Entry<String, Results> me : scores.entrySet())
+			{
+				System.out.println(me.getKey()+" (place: frequency)");
+				Results r = me.getValue();
+				
+				for(int i = 0; i < numSnakes; ++i)
+				{
+					System.out.println("\t"+(i+1)+": "+r.getFreq(i)+" times");
+				}
+			}
+			
+			System.out.println("DONE");
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error: " + e);
+		}
+	}
+	
+	private class Results
+	{
+		private int[] placements;
+		
+		public Results(int numSnakes)
+		{
+			placements = new int[numSnakes];
+			
+			for(int i = 0; i < placements.length; ++i)
+			{
+				placements[i] = 0;
 			}
 		}
 		
-		System.out.println("DONE");
+		void addResult(int i)
+		{
+			placements[i] += 1;
+		}
+		
+		int getFreq(int place)
+		{
+			return placements[place];
+		}
 	}
 }
 
