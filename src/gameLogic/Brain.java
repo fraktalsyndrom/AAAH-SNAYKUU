c2c7bdfb8b3cce4aa5a4164dc15b44acbe505106
Sixten Hilborn <hilborn90@hotmail.com>
2011-11-12 17:48:51 +0100

    Updating some documentation

--- src/gameLogic/Brain.java
+++ src/gameLogic/Brain.java
@@ -3,8 +3,8 @@ package gameLogic;
 /**
  * This is the interface a participant needs to implement into his or her code.
  * A single method, <code>getNextMove</code>, is required, and this is the method
- * that will be called by {@link Session} each turn in order to ask the participant's snake
- * which direction it wishes to move in.
+ * that will be called by {@link Session} (the game engine) each turn in order to
+ * ask the participant's snake which direction it wishes to move in.
  *
  * @author	Sixten Hilborn
  * @author	Arian Jafari
@@ -13,7 +13,8 @@ package gameLogic;
 public interface Brain
 {
 	/**
-	 * Returns which direction this brain wishes its snake to move in. Given a {@link GameState}
+	 * Returns which direction this brain wishes its snake to move in. This function is called
+	 * by the game engine ({@link Session}) every turn. Given a {@link GameState}
 	 * and a reference to the brain's snake, the brain should somehow calculate which direction
 	 * it finds is optimal to move towards. If the Direction returned by this method is a 180 degree
 	 * turn in comparison to the snake's current direction, its direction will simply remain unchanged.
@@ -21,8 +22,8 @@ public interface Brain
 	 * previous direction.
 	 * 
 	 * @param	yourSnake	The snake belonging to this brain.
-	 * @param	gamestate	The current state of the game.
+	 * @param	gameState	The current state of the game.
 	 * @return	The direction in which the brain wants its snake to move next.
 	 */
-	public Direction getNextMove(Snake yourSnake, GameState gamestate);
+	public Direction getNextMove(Snake yourSnake, GameState gameState);
 }
