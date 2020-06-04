1d365315aaff9022dac68b7510243c31932231a9
AdamOstgaard <adam.ostgaard@gmail.com>
2020-06-04 14:56:05 +0200

    Add code skeleton for bot

---
+++ bot/TungurBottur.java
@@ -0,0 +1,14 @@
+package bot;
+
+import gameLogic.Brain;
+import gameLogic.Direction;
+import gameLogic.GameState;
+import gameLogic.Snake;
+
+public class TungurBottur implements Brain {
+	@Override
+	public Direction getNextMove(Snake yourSnake, GameState gameState) {
+
+		return null;
+	}
+}
